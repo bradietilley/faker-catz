0d85d210c2910cba1aad765b97b445368349e562
Bradie Tilley <44430471+bradietilley@users.noreply.github.com>
2024-04-12 15:20:08 +0800

    test: ignore environments that don't support mt_srand

--- tests/Unit/CatzTest.php
+++ tests/Unit/CatzTest.php
@@ -63,14 +63,31 @@ test('catz can load all cat pics', function () {
 
 test('catz can seed consistent results', function () {
     /**
-     * Pools are randomised each time by default
+     * Pools are randomised each time it's loaded by default
      */
     expect(catz()->load()->pool())->not->toBe(catz()->load()->pool());
 
     catz()->seed(4353454536);
 
+    mt_srand(3453578934543);
+    $expect = mt_rand(100, 1000);
+    mt_srand(); // reset
+
+    mt_srand(3453578934543);
+    $actual = mt_rand(100, 1000);
+    mt_srand(); // reset
+
+    if ($expect !== $actual) {
+        // We're dealing with an env that doesn't support mt_srand
+        $this->markTestSkipped('mt_srand does not work');
+
+        return;
+    }
+
+    // Otherwise mt_srand works
+
     /**
-     * Pools are randomised using the seed
+     * Pools are randomised using the seed and thus aren't random anymore
      */
     expect(catz()->load()->pool())->toBe(catz()->load()->pool());
 });
